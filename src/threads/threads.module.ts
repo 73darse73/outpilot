32ef78c57c4fb371d77b71ca8fb5eeb77c136be1
73darse73 <73darse73@gmail.com>
2025-06-25 20:34:27 +0900

    feat: ThreadsModuleにThreadsControllerを追加

--- src/threads/threads.module.ts
+++ src/threads/threads.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ThreadsService } from './threads.service';
 import { PrismaModule } from '../prisma/prisma.module';
+import { ThreadsController } from './threads.controller';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [ThreadsController],
   providers: [ThreadsService],
   exports: [ThreadsService],
 })
